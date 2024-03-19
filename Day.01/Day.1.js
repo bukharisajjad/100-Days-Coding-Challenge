08ed65d5b5f699ba64d076b9f08b66520e597b93
Sajjad Ali Shah <164080854+bukharisajjad@users.noreply.github.com>
2024-03-20 01:59:42 +0500

    Add files via upload

---
+++ Day.01/Day.1.js
@@ -0,0 +1,6 @@
+/*I have made a variable with my name.*/
+var myname = "Sajjad";
+// console.log(myname)
+myname = "Ali";
+console.log(myname);
+var myage = 300;
---
+++ Day.01/Day.1.ts
@@ -0,0 +1,10 @@
+/*I have made a variable with my name.*/
+
+let myname: string = "Sajjad"
+// console.log(myname)
+
+myname = "Ali"
+console.log(myname)
+
+
+let myage: number = 300
